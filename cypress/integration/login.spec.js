b33429cc8d87b5e47fbebefe0f5be5fe8c1380cf
jagannathangit <jagannathann@yahoo.com>
2022-02-20 01:10:56 -0600

    Added Logout Scenario
    Added Logout Scenario

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -5,7 +5,7 @@ describe("Automated UI test suite around the login page.", () => {
   beforeEach(() => {
     cy.visit("/login");
   });
-  it.only("should be able to log in the Test Site and show  home page information", () => {
+  it("should be able to log in the Test Site and show  home page information", () => {
     cy.get('[data-cy="username__input"]').clear().type(username);
     cy.get('[data-cy="password__input"]')
       .clear()
@@ -14,12 +14,20 @@ describe("Automated UI test suite around the login page.", () => {
     cy.get('[data-cy="submit__btn"]').click();
 
     cy.url().should("include", "welcome");
-    // cy.get(":nth-child(1) > .nav-item > .nav-label").should(
-    //   "contain",
-    //   "Workspaces"
-    // );
     cy.get(".ngx-dropdown-toggle").click();
     cy.get(".profile-title > span").should("contain", "Jagannathan");
+    cy.get(".btn.logout-button.pull-right")
+      .should("contain", "Log Out")
+      .click();
+
+    cy.get("h1[data-cy='logout__msg']").should(
+      "contain",
+      "You have successfully logged out"
+    );
+    cy.get("[data-cy='return__btn']")
+      .should("contain", "Return to Swimlane")
+      .click();
+    cy.url().should("contain", Cypress.config().baseUrl);
   });
   it("should not be able to log in with incorrect password", () => {
     cy.get('[data-cy="username__input"]').clear().type(username);
@@ -31,7 +39,7 @@ describe("Automated UI test suite around the login page.", () => {
     cy.get(".login-error").should("contain", "Login failed.");
     cy.url().should("contain", Cypress.config().baseUrl);
   });
-  it.only("should not be able to log in with incorrect username", () => {
+  it("should not be able to log in with incorrect username", () => {
     cy.get('[data-cy="username__input"]').clear().type("incorrect-username");
     cy.get('[data-cy="password__input"]')
       .clear()
@@ -41,7 +49,7 @@ describe("Automated UI test suite around the login page.", () => {
     cy.get(".login-error").should("contain", "Login failed.");
     cy.url().should("contain", Cypress.config().baseUrl);
   });
-  it.only("should not be able to log in with empty username and empty password", () => {
+  it("should not be able to log in with empty username and empty password", () => {
     cy.get("[data-cy=username__input]").click().should("have.value", "");
     cy.get("[data-cy=password__input]").click().should("have.value", "");
     cy.get('[data-cy="submit__btn"]').should("be.disabled");
